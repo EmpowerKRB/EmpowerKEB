64ad174fab0dc05f04527e320f590856c89b86c9
kristopher.brookshir <kristopher.brookshir@LAPTOP-Q752T5F>
2023-05-16 14:09:18 -0400

    Add project files.

---
+++ EmpowerKRB_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmpowerKRB_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("EmpowerKRB_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("D0EC638F-CA64-400A-8D49-263D3058E556")]
+[assembly: AssemblyVersion("1.0.0.0")]
